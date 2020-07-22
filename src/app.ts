765019a78377a6d98d3c75f479a76e030bf66a13
Jakub Sukiennik <kuba1207710@gmail.com>
2020-07-22 22:00:07 +0200

    seting up server

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,24 @@
 import 'reflect-metadata';
-import { ApolloServer, gql } from 'apollo-server-express';
-import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import * as express from 'express';
 import { createConnection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
-
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
-};
-async () => {
+import { HelloWorldResolver } from './resolvers/HelloWorldResolver';
+(async () => {
     const app = express();
 
     await createConnection();
 
-    const server = new ApolloServer({ typeDefs, resolvers });
-    server.applyMiddleware({ app, cors: false });
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => `Sever running on port ${PORT}`);
-};
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloWorldResolver],
+        }),
+        context: ({ req, res }) => ({ req, res }),
+    });
+
+    apolloServer.applyMiddleware({ app, cors: false });
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+        console.log(`server started at http://localhost:${port}/graphql`);
+    });
+})();
