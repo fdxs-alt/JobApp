f18b0da40bdaab166e544cdf041a9154e120c39d
Jakub Sukiennik <kuba1207710@gmail.com>
2020-07-23 16:35:21 +0200

    session

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import * as express from 'express';
 import { createConnection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
 import { AuthResolver } from './resolvers/AuthResolver';
+import { redis } from './redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+
 (async () => {
   const app = express();
 
@@ -15,6 +19,25 @@ import { AuthResolver } from './resolvers/AuthResolver';
     }),
     context: ({ req, res }) => ({ req, res }),
   });
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: 'qid',
+      secret: 'aslkdfjoiq12312',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    }),
+  );
 
   apolloServer.applyMiddleware({ app, cors: false });
   const PORT = process.env.PORT || 5000;
